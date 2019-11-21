32d1738178d2147c5a623e10ae19c713195b3dd7
Jocelyne Herrera <42578429+chicjoss@users.noreply.github.com>
2019-11-21 17:01:34 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.967070,  lng:-5.663658},
+    zoom: 18
+  });
+	
+var image = "photos/pin.png";
+  var spainMarker = new google.maps.Marker({
+    position: {lat: 40.960607,len: -5.665813},
+    map: map,
+    icon: image
+  });
+}
+window.addEventListener('onload',initMap);
