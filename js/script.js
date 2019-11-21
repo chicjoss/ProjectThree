3857dd8b4b857ee414c79e72e8daef9c9614ed46
Jocelyne Herrera <42578429+chicjoss@users.noreply.github.com>
2019-11-21 14:27:17 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.967070,  lng:-5.663658, },
+    center: {(lat: 40.967070,  lng:-5.663658)},
     zoom: 18
   });
 	
 var image = "photos/pin.png";
   var spainMarker = new google.maps.Marker({
-    position: {lat: 40.960607,len: -5.665813},
+    position: {(lat: 40.960607,len: -5.665813)},
     map: map,
     icon: image
   });
