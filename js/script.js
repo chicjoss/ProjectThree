be530ff04af308bde04c1fd8e712539a723b9d36
Jocelyne Herrera <jherrera2@hawk.iit.edu>
2019-11-21 18:47:43 -0600

    added a slideshow and made it look pretty

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ var image = "photos/pin.png";
   });
 }
 window.addEventListener('onload',initMap);
+
+/*Added a Javascript slideshow for fun*/
+var start=0;
+slideshow();
+function slideshow(){
+  var beginning;
+  var grab_html_element=document.getElementsByClassName("slides");
+  for(beginning=0;beginning<grab_html_element.length;beginning++){
+    grab_html_element[beginning].style.display="none";
+  }
+  start++;
+  if(start>grab_html_element.length){start=1}
+  grab_html_element[start-1].style.display="block";
+  setTimeout(slideshow,5000);
+}
